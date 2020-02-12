e019ca49170918e1030660091537160612fc4fb5
Ansh Godha <ag759@cornell.edu>
2020-02-12 18:49:11 -0500

    Initial setup for gym views

---
+++ Campus Density/GymCells/GymDensityCell.swift
@@ -0,0 +1,83 @@
+//
+//  GymDensityCell.swift
+//  Campus Density
+//
+//  Created by Ansh Godha on 12/02/20.
+//  Copyright © 2020 Cornell DTI. All rights reserved.
+//
+
+import UIKit
+
+class GymDensityCell: UICollectionViewCell {
+    
+    var cardioView: UIView!
+    var weightView: UIView!
+    var cardioLabel: UILabel!
+    var weightLabel: UILabel!
+    
+    let padding: CGFloat = 20
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+    }
+    
+    func setUpViews() {
+        cardioView = UIView()
+        cardioView.layer.cornerRadius = cardioView.frame.height/10
+        contentView.addSubview(cardioView)
+        
+        weightView = UIView()
+        weightView.layer.cornerRadius = weightView.frame.height/10
+        contentView.addSubview(weightView)
+        
+        cardioLabel = makeLabel(withText: "Cardio")
+        cardioView.addSubview(cardioLabel)
+        
+        weightLabel = makeLabel(withText: "Weights")
+        weightView.addSubview(weightLabel)
+    }
+    
+    func makeLabel(withText text: String) -> UILabel {
+        let label = UILabel()
+        label.adjustsFontSizeToFitWidth = true
+        label.textColor = .grayishBrown
+        label.textAlignment = .center
+        label.font = .sixteenBold
+        label.text = text
+        return label
+    }
+    
+    func setUpConstraints() {
+        
+        let boxDimensions = (frame.width - 3 * padding)/2
+        
+        cardioView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(padding)
+            make.height.equalTo(boxDimensions)
+            make.width.equalTo(boxDimensions)
+            make.left.equalToSuperview().offset(padding)
+        }
+        
+        weightView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(padding)
+            make.height.equalTo(boxDimensions)
+            make.width.equalTo(boxDimensions)
+            make.right.equalToSuperview().inset(padding)
+        }
+        
+        cardioLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(5)
+            make.left.equalToSuperview().offset(5)
+        }
+        
+        weightLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(5)
+            make.left.equalToSuperview().offset(5)
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Campus Density/GymModels/GymDensityModel.swift
@@ -0,0 +1,35 @@
+//
+//  GymDensityModel.swift
+//  Campus Density
+//
+//  Created by Ansh Godha on 12/02/20.
+//  Copyright © 2020 Cornell DTI. All rights reserved.
+//
+
+import Foundation
+import IGListKit
+
+class GymDensityModel {
+    var currentCardioCount: Int
+    var maxCardioCount: Int
+    var currentWeightCount: Int
+    let identifier = UUID().uuidString
+    
+    init (currentCardioCount: Int, maxCardioCount: Int, currentWeightCount:Int) {
+        self.currentCardioCount = currentCardioCount
+        self.maxCardioCount = maxCardioCount
+        self.currentWeightCount = currentWeightCount
+    }
+}
+
+extension GymDensityModel: ListDiffable {
+    func diffIdentifier() -> NSObjectProtocol {
+        return identifier as NSString
+    }
+    
+    func isEqual(toDiffableObject object: ListDiffable?) -> Bool {
+        if object === self {return true}
+        guard let object = object as? GymDensityModel else {return false}
+        return object.identifier == identifier
+    }
+}
