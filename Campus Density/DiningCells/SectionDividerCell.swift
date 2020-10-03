0416b029553c73bbad0c3e7030becae844f62f42
mihika123 <71369151+mihika123@users.noreply.github.com>
2020-10-04 01:26:27 +0530

    Add files via upload

---
+++ Campus Density/DiningCells/SectionDividerCell.swift
@@ -0,0 +1,34 @@
+//
+//  SectionDividerCell.swift
+//  Campus Density
+//
+//  Created by Mihikaa Goenka on 9/29/20.
+//  Copyright © 2020 Cornell DTI. All rights reserved.
+//
+
+import UIKit
+
+class SectionDividerCell: UICollectionViewCell {
+    
+    // MARK: - View vars
+    var line: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        line = UIView()
+        line.backgroundColor = .densityLightGray
+        addSubview(line)
+        
+        line.snp.makeConstraints { make in
+            make.width.height.equalToSuperview()
+            make.center.equalToSuperview()
+        }
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
