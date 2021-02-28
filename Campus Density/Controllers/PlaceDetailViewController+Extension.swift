ab5439cdc75b4f8489f689e5768a0d4439f80230
Changyuan Lin <coreballium@outlook.com>
2021-02-28 14:17:49 -0500

    Last Updated Cell on Detail View

--- Campus Density/Controllers/PlaceDetailViewController+Extension.swift
+++ Campus Density/Controllers/PlaceDetailViewController+Extension.swift
@@ -53,6 +53,8 @@ extension PlaceDetailViewController: ListAdapterDataSource {
             SpaceModel(space: Constants.smallPadding),
             AvailabilityHeaderModel(),
             SpaceModel(space: Constants.smallPadding),
+            LastUpdatedTextModel(lastUpdated: lastUpdatedTime),
+            SpaceModel(space: linkTopOffset),
             AvailabilityInfoModel(place: place), // TODO: look into only passing what's necessary
             SpaceModel(space: linkTopOffset),
             FormLinkModel(lastUpdated: lastUpdatedTime),
@@ -102,6 +104,9 @@ extension PlaceDetailViewController: ListAdapterDataSource {
         } else if object is AvailabilityHeaderModel {
             let availabilityHeaderModel = object as! AvailabilityHeaderModel
             return AvailabilityHeaderSectionController(headerModel: availabilityHeaderModel)
+        } else if object is LastUpdatedTextModel {
+            let lastUpdatedTextModel = object as! LastUpdatedTextModel
+            return LastUpdatedTextSectionController(lastUpdatedTextModel: lastUpdatedTextModel, style: .detail)
         } else {
             let menuHeaderModel = object as! MenuHeaderModel
             return MenuHeaderSectionController(menuHeaderModel: menuHeaderModel)
